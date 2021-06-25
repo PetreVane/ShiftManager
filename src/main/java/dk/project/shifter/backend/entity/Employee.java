f8f47d96a30cfad708acb1f4fe8fea7f9b6d25a5
petre <petre.vane@gmail.com>
2021-06-25 22:48:05 +0200

    Added relationship mappings
    Signed-off-by: petre <petre.vane@gmail.com>

--- src/main/java/dk/project/shifter/backend/entity/Employee.java
+++ src/main/java/dk/project/shifter/backend/entity/Employee.java
@@ -2,15 +2,15 @@ package dk.project.shifter.backend.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.LinkedList;
+import java.util.List;
 
 @Data
 @Entity
+@Table(name = "employee")
 public class Employee extends AbstractEntity {
 
     public enum HoursType {
@@ -38,5 +38,16 @@ public class Employee extends AbstractEntity {
     @Enumerated
     @NotNull
     private Employee.HoursType hoursType;
+
+    @OneToMany(mappedBy = "employee", cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Shift> shiftList;
+
+    public void addShift(Shift shift) {
+        if (shiftList == null) {
+            shiftList = new LinkedList<>();
+        }
+        shiftList.add(shift);
+        shift.setEmployee(this);
+    }
 
 }
