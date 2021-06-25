01bff2510f2f984a815d94a8383773e4848348aa
petre <petre.vane@gmail.com>
2021-06-25 22:49:08 +0200

    Added validation and table relationship + new service
    Signed-off-by: petre <petre.vane@gmail.com>

--- src/main/java/dk/project/shifter/backend/entity/Shift.java
+++ src/main/java/dk/project/shifter/backend/entity/Shift.java
@@ -1,17 +1,40 @@
 package dk.project.shifter.backend.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
+@NoArgsConstructor
 @Entity
-public class Shift  extends AbstractEntity {
+public class Shift extends AbstractEntity {
 
+    @NonNull
+    @NotEmpty
     private LocalDate date;
+
+    @NotNull
+    @NotEmpty
     private LocalTime startingTime;
+
+    @NotNull
+    @NotEmpty
     private LocalTime endingTime;
+
+    @NotNull
+    @NotEmpty
     private LocalTime pauseDuration;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
 }
---
+++ src/main/java/dk/project/shifter/backend/repository/ShiftRepository.java
@@ -0,0 +1,7 @@
+package dk.project.shifter.backend.repository;
+
+
+import dk.project.shifter.backend.entity.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftRepository extends JpaRepository<Shift, Long> { }
