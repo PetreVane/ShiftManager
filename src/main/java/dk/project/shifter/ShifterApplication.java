7bdf7b2b6f0c8b1dc13a5189168028c32512fcf1
Petre Vane <>
2021-06-25 12:38:25 +0200

    first init

---
+++ src/main/java/dk/project/shifter/ShifterApplication.java
@@ -0,0 +1,13 @@
+package dk.project.shifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShifterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/project/shifter/ShifterApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.project.shifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
