d1fb96b850eb2c83ab70221139310304ca39ee03
petre <petre.vane@gmail.com>
2021-06-27 16:48:32 +0200

    Added test data
    Signed-off-by: petre <petre.vane@gmail.com>

--- src/main/java/dk/project/shifter/backend/service/ShiftService.java
+++ src/main/java/dk/project/shifter/backend/service/ShiftService.java
@@ -5,6 +5,9 @@ import dk.project.shifter.backend.repository.ShiftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -17,6 +20,7 @@ public class ShiftService {
 
     @Autowired
     public ShiftService(ShiftRepository shiftRepository) {
+        logger.info("INFO ======>>> Called bean constructor for ShiftService");
         this.shiftRepository = shiftRepository;
     }
 
@@ -38,6 +42,21 @@ public class ShiftService {
 
     public void deleteShiftById(Long id) {
         shiftRepository.deleteById(id);
+    }
+
+    public Shift getShift(Long id) {
+        return shiftRepository.getOne(id);
+    }
+
+    @PostConstruct
+    public void addTestData() {
+        Shift sundayShift = new Shift();
+        sundayShift.setShiftDate(LocalDate.now());
+        sundayShift.setStartingTime(LocalTime.of(10, 00));
+        sundayShift.setHadBreak(true);
+        sundayShift.setEndingTime(LocalTime.of(17, 30));
+
+        shiftRepository.save(sundayShift);
     }
 
 }
