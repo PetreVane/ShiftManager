e9e273a8ee167cf46293cd4158113564a9bc944e
petre <petre.vane@gmail.com>
2021-06-27 16:49:20 +0200

    Added validation message
    Signed-off-by: petre <petre.vane@gmail.com>

--- src/main/java/dk/project/shifter/backend/entity/Company.java
+++ src/main/java/dk/project/shifter/backend/entity/Company.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
 @Entity
 public class Company extends AbstractEntity {
 
+    @NotNull(message = "Company name needs at least 3 charactes")
     private String name;
 
     @OneToMany(mappedBy = "company", fetch = FetchType.EAGER)
