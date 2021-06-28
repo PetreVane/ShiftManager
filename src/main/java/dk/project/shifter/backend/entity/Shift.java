de72a9e068603127b044fdfc3e8b8d1a1001b171
Petre Vane <>
2021-06-28 14:18:18 +0200

    Removed comments

--- src/main/java/dk/project/shifter/backend/entity/Shift.java
+++ src/main/java/dk/project/shifter/backend/entity/Shift.java
@@ -1,38 +1,41 @@
 package dk.project.shifter.backend.entity;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Data
-@NoArgsConstructor
 @Entity
 public class Shift extends AbstractEntity {
 
-//    @NonNull
-//    @NotEmpty
+
     @Column(name = "shift_date")
     private LocalDate shiftDate;
 
-//    @NotNull
-//    @NotEmpty
+
     @Column(name = "starting_time")
     private LocalTime startingTime;
 
-//    @NotNull
-//    @NotEmpty
+
     @Column(name = "ending_time")
     private LocalTime endingTime;
 
-//    @NotNull
-//    @NotEmpty
+
     @Column(name = "had_break")
     private boolean hadBreak;
 
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "employee_id")
     private Employee employee;
+
+    public Shift() { }
+
+    public Shift(LocalDate shiftDate, LocalTime startingTime, LocalTime endingTime, boolean hadBreak) {
+        this.shiftDate = shiftDate;
+        this.startingTime = startingTime;
+        this.endingTime = endingTime;
+        this.hadBreak = hadBreak;
+    }
 }
--- src/main/java/dk/project/shifter/backend/repository/ShiftRepository.java
+++ src/main/java/dk/project/shifter/backend/repository/ShiftRepository.java
@@ -4,4 +4,5 @@ package dk.project.shifter.backend.repository;
 import dk.project.shifter.backend.entity.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface ShiftRepository extends JpaRepository<Shift, Long> { }
