bf1df30ef8095f1067d4fb9d39b6d31f8697c824
Petre Vane <>
2021-06-28 17:33:18 +0200

    Added new column to grid

--- src/main/java/dk/project/shifter/ui/MainView.java
+++ src/main/java/dk/project/shifter/ui/MainView.java
@@ -3,6 +3,7 @@ package dk.project.shifter.ui;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import dk.project.shifter.backend.entity.Company;
 import dk.project.shifter.backend.entity.Shift;
 import dk.project.shifter.backend.service.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MainView extends VerticalLayout {
 
     private final Grid<Shift> grid = new Grid<>(Shift.class);
-    private ShiftService shiftService;
+    private final ShiftService shiftService;
 
     public MainView(@Autowired ShiftService shiftService) {
         this.shiftService = shiftService;
@@ -37,8 +38,12 @@ public class MainView extends VerticalLayout {
     private void configureGrid() {
         grid.addClassName("contact-grid"); // grid css styling
         grid.setSizeFull();
-        grid.setColumns("shiftDate", "startingTime", "endingTime", "hadBreak");
-//        grid.removeColumn("employee");
+        grid.removeColumnByKey("company");
+        grid.setColumns("shiftDate", "startingTime", "endingTime", "hadBreak", "hoursType");
+        grid.addColumn(shift -> {
+            Company company = shift.getCompany();
+            return company == null ? "-" : company.getName();
+        }).setHeader("Company");
     }
 
     private void updateGridContent() {
