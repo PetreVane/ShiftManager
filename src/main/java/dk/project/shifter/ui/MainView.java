ac7c3f10c814d4d2f5181e6eaea2248fd7bcf2ee
Petre Vane <>
2021-06-29 14:02:21 +0200

    - removed Company Column; - renamed method; - added logic for saving shifts

--- src/main/java/dk/project/shifter/ui/MainView.java
+++ src/main/java/dk/project/shifter/ui/MainView.java
@@ -1,11 +1,12 @@
 package dk.project.shifter.ui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.Route;
-import dk.project.shifter.backend.entity.Company;
 import dk.project.shifter.backend.entity.Shift;
 import dk.project.shifter.backend.service.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,20 +45,32 @@ public class MainView extends VerticalLayout {
         SplitLayout splitLayout = new SplitLayout();
         splitLayout.setSizeFull();
         shiftForm = new ShiftForm(splitLayout, grid);
+        shiftForm.addListener(ShiftForm.SaveEvent.class, this::saveShift);
+        shiftForm.addListener(ShiftForm.DeleteEvent.class, this::deleteShift);
+        shiftForm.addListener(ShiftForm.CancelEvent.class, e -> this.clearEditor());
 
         updateGridContent();
-        add(splitLayout);
+        add(splitLayout, getToolbar()); // getToolbar()
     }
 
+    private HorizontalLayout getToolbar() {
+        Button addShiftButton = new Button("Add new shift");
+        addShiftButton.addClickListener(click -> addShift());
+        HorizontalLayout toolBar = new HorizontalLayout(addShiftButton);
+        toolBar.addClassName("toolbar");
+        return toolBar;
+    }
+
+    // grid
     private void configureGrid() {
         grid.addClassName("contact-grid"); // grid css styling
         grid.setSizeFull();
         grid.removeColumnByKey("company");
         grid.setColumns("shiftDate", "startingTime", "endingTime", "hadBreak", "hoursType");
-        grid.addColumn(shift -> {
-            Company company = shift.getCompany();
-            return company == null ? "-" : company.getName();
-        }).setHeader("Company");
+//        grid.addColumn(shift -> {
+//            Company company = shift.getCompany();
+//            return company == null ? "-" : company.getName();
+//        }).setHeader("Company");
         grid.getColumns().forEach(column -> column.setAutoWidth(true));
 
         // shows selected row
@@ -68,20 +81,45 @@ public class MainView extends VerticalLayout {
         grid.setItems(shiftService.findAll());
     }
 
+    // editor
+    private void clearEditor() {
+        shiftForm.setShift(null);
+        shiftForm.setVisible(false);
+        shiftForm.removeClassName("editing");
+    }
+
+    public void addShift() {
+        Shift shift = new Shift();
+        editShift(shift);
+        grid.asSingleSelect().clear();
+    }
+
     private void editShift(Shift shift) {
         if (shift == null) {
-            closeEditor();
+            clearEditor();
         } else {
             shiftForm.setShift(shift);
             shiftForm.setVisible(true);
             addClassName("editing");
         }
+
     }
 
-    private void closeEditor() {
-        shiftForm.setShift(null);
-        shiftForm.setVisible(false);
-        shiftForm.removeClassName("editing");
+    private void saveShift(ShiftForm.ShiftFormEvent event) {
+        shiftService.saveShift(event.getShift());
+        updateGridContent();
+        clearEditor();
+    }
+
+    private void deleteShift(ShiftForm.ShiftFormEvent event) {
+        Shift shiftToBeDeleted = event.getShift();
+        if (shiftToBeDeleted == null) {
+            System.out.println("Shift is null");
+        } else  {
+            shiftService.deleteShift(shiftToBeDeleted);
+            updateGridContent();
+            clearEditor();
+        }
     }
 
 }
