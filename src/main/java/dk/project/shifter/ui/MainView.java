0c20dff156b59f43eb5f5e98b2e734c1ac1b91f6
Petre Vane <>
2021-06-29 15:00:26 +0200

    Refactored

--- src/main/java/dk/project/shifter/ui/MainView.java
+++ src/main/java/dk/project/shifter/ui/MainView.java
@@ -3,26 +3,15 @@ package dk.project.shifter.ui;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.router.Route;
 import dk.project.shifter.backend.entity.Shift;
 import dk.project.shifter.backend.service.ShiftService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * A example of Vaadin view class.
- * <p>
- * To implement a Vaadin view just extend any Vaadin component and
- * use @Route annotation to announce it in a URL as a Spring managed
- * bean.
- * Use the @PWA annotation make the application installable on phones,
- * tablets and some desktop browsers.
- * <p>
- * A new instance of this class is created for every new user and every
- * browser tab/window.
- */
+
 @Route("")
 @CssImport("./styles/shared-styles.css")
 public class MainView extends VerticalLayout {
@@ -42,15 +31,18 @@ public class MainView extends VerticalLayout {
         setSizeFull(); // applies to the mainView
         configureGrid();
 
-        SplitLayout splitLayout = new SplitLayout();
-        splitLayout.setSizeFull();
-        shiftForm = new ShiftForm(splitLayout, grid);
+        shiftForm = new ShiftForm();
+        Div content = new Div(grid, shiftForm);
+        content.addClassName("content");
+        content.setSizeFull();
+
+
         shiftForm.addListener(ShiftForm.SaveEvent.class, this::saveShift);
         shiftForm.addListener(ShiftForm.DeleteEvent.class, this::deleteShift);
         shiftForm.addListener(ShiftForm.CancelEvent.class, e -> this.clearEditor());
 
         updateGridContent();
-        add(splitLayout, getToolbar()); // getToolbar()
+        add(getToolbar(), content); // getToolbar()
     }
 
     private HorizontalLayout getToolbar() {
@@ -67,10 +59,6 @@ public class MainView extends VerticalLayout {
         grid.setSizeFull();
         grid.removeColumnByKey("company");
         grid.setColumns("shiftDate", "startingTime", "endingTime", "hadBreak", "hoursType");
-//        grid.addColumn(shift -> {
-//            Company company = shift.getCompany();
-//            return company == null ? "-" : company.getName();
-//        }).setHeader("Company");
         grid.getColumns().forEach(column -> column.setAutoWidth(true));
 
         // shows selected row
@@ -102,7 +90,6 @@ public class MainView extends VerticalLayout {
             shiftForm.setVisible(true);
             addClassName("editing");
         }
-
     }
 
     private void saveShift(ShiftForm.ShiftFormEvent event) {
