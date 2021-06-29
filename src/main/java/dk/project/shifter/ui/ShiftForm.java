fab127943fdfa8ed309a24c763a51c40a97a95c9
Petre Vane <>
2021-06-29 15:00:12 +0200

    Refactored

--- src/main/java/dk/project/shifter/ui/ShiftForm.java
+++ src/main/java/dk/project/shifter/ui/ShiftForm.java
@@ -1,16 +1,16 @@
 package dk.project.shifter.ui;
 
-import com.vaadin.flow.component.*;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.timepicker.TimePicker;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
@@ -34,43 +34,19 @@ public class ShiftForm extends FormLayout {
     Binder<Shift> binder = new BeanValidationBinder<>(Shift.class);
     private Shift shift;
 
+    public ShiftForm() {
+        addClassName("contact-form");
+        binder.bindInstanceFields(this);
+        add(shiftDate, startingTime, endingTime, hadBreak, configureHoursTypeCombo(), createButtonLayout());
+    }
+
     public void setShift(Shift shift) {
         this.shift = shift;
         binder.readBean(shift);
     }
 
 
-    public <T> ShiftForm(SplitLayout splitLayout, Grid<T> grid) {
-        addClassName("contact-form");
-        binder.bindInstanceFields(this);
-
-        createEditorLayout(splitLayout);
-        createGridLayout(splitLayout, grid);
-    }
-
-    private void createEditorLayout(SplitLayout splitLayout) {
-        Div editorLayoutDiv = new Div();
-        editorLayoutDiv.setClassName("flex flex-col");
-        editorLayoutDiv.setWidth("400px");
-
-        Div editorDiv = new Div();
-        editorDiv.setClassName("p-l flex-grow");
-        editorLayoutDiv.add(editorDiv);
-
-        FormLayout formLayout = new FormLayout();
-        Component[] fields = new Component[]{shiftDate, startingTime, endingTime, hadBreak, configureHoursTypeCombo()};
-
-        for (Component field : fields) {
-            ((HasStyle) field).addClassName("full-width");
-        }
-        formLayout.add(fields);
-        editorDiv.add(formLayout);
-        createButtonLayout(editorLayoutDiv);
-
-        splitLayout.addToSecondary(editorLayoutDiv);
-    }
-
-    private void createButtonLayout(Div editorLayoutDiv) {
+    private Component createButtonLayout() {
         HorizontalLayout buttonLayout = new HorizontalLayout();
         buttonLayout.setClassName("w-full flex-wrap bg-contrast-5 py-s px-l");
         buttonLayout.setSpacing(true);
@@ -89,44 +65,28 @@ public class ShiftForm extends FormLayout {
         deleteButton.addClickListener(event -> fireEvent(new DeleteEvent(this, shift)));
         cancelButton.addClickListener(event -> fireEvent(new CancelEvent(this)));
 
-
+        binder.addStatusChangeListener(evt -> saveButton.setEnabled(binder.isValid()));
 
         buttonLayout.add(saveButton, cancelButton, deleteButton);
-        editorLayoutDiv.add(buttonLayout);
+        return buttonLayout;
     }
 
     private void validateAndSave() {
         try {
             binder.writeBean(shift);
             fireEvent(new SaveEvent(this, shift));
-
         } catch (ValidationException e) {
             e.printStackTrace();
         }
     }
 
-    private void createGridLayout(SplitLayout splitLayout, Grid grid) {
-        Div wrapper = new Div();
-        wrapper.setId("grid-wrapper");
-        wrapper.setWidthFull();
-        splitLayout.addToPrimary(wrapper);
-        wrapper.add(grid);
-    }
-
-    private ComboBox<Shift.HoursType> configureHoursTypeCombo() {
+    private Component configureHoursTypeCombo() {
         var types = Shift.HoursType.values();
         hoursType.setItems(types);
         return hoursType;
     }
 
-    private void refreshGrid(Grid grid) {
-        grid.select(null);
-        grid.getDataProvider().refreshAll();
-    }
-
-
     public static abstract class ShiftFormEvent extends ComponentEvent<ShiftForm> {
-
         private final Shift shift;
 
         protected ShiftFormEvent(ShiftForm source, Shift shift) {
@@ -160,5 +120,43 @@ public class ShiftForm extends FormLayout {
     public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType, ComponentEventListener<T> listener) {
         return getEventBus().addListener(eventType, listener);
     }
-
 }
+
+
+    /*
+    private void createEditorLayout(SplitLayout splitLayout) {
+        Div editorLayoutDiv = new Div();
+        editorLayoutDiv.setClassName("flex flex-col");
+        editorLayoutDiv.setWidth("400px");
+
+        Div editorDiv = new Div();
+        editorDiv.setClassName("p-l flex-grow");
+        editorLayoutDiv.add(editorDiv);
+
+        FormLayout formLayout = new FormLayout();
+        Component[] fields = new Component[]{shiftDate, startingTime, endingTime, hadBreak, configureHoursTypeCombo()};
+
+        for (Component field : fields) {
+            ((HasStyle) field).addClassName("full-width");
+        }
+        formLayout.add(fields);
+        editorDiv.add(formLayout);
+        createButtonLayout(editorLayoutDiv);
+
+        splitLayout.addToSecondary(editorLayoutDiv);
+    }
+
+    private void createGridLayout(SplitLayout splitLayout, Grid grid) {
+        Div wrapper = new Div();
+        wrapper.setId("grid-wrapper");
+        wrapper.setWidthFull();
+        splitLayout.addToPrimary(wrapper);
+        wrapper.add(grid);
+    }
+
+    private void refreshGrid(Grid grid) {
+        grid.select(null);
+        grid.getDataProvider().refreshAll();
+    }
+
+     */
