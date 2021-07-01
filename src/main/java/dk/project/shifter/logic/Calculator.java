569e2da260241e8521d4e694f35e6ff2491ceaf8
Petre Vane <>
2021-07-01 21:34:39 +0200

    Refactored; removed comments
    Signed-off-by: Petre Vane <>

--- src/main/java/dk/project/shifter/logic/Calculator.java
+++ src/main/java/dk/project/shifter/logic/Calculator.java
@@ -7,7 +7,7 @@ import java.time.LocalTime;
 
 public class Calculator {
 
-    public static LocalTime getWorkedHoursFor(Shift shift) {
+    public static Duration getWorkedHoursFor(Shift shift) {
         var totalHours = shift.getEndingTime().minusHours(shift.getStartingTime().getHour());
         if (shift.hadBreak()) { totalHours = totalHours.minusMinutes(30); }
 
@@ -15,23 +15,14 @@ public class Calculator {
         totalHours = totalHours.minusMinutes(totalMinutes);
         LocalTime result = LocalTime.of(totalHours.getHour(), totalMinutes);
         System.out.println("Total hours worked: " + result);
-        countDuration(result);
-        return result;
+        return countDuration(result);
     }
 
-//    public static double countWorkedHours(LocalTime totalHoursForOneDay) {
-//        double totalHours = totalHoursForOneDay.getHour();
-//        double totalMinutes = totalHoursForOneDay.getMinute();
-//        double hoursConverted = totalMinutes / 60;
-//        var extraMinutes = totalMinutes % 60;
-//        System.out.println("Total worked hours is " + totalHours + " hours and " + extraMinutes + " minutes");
-//        return totalHours = totalHours + hoursConverted;
-//    }
 
-    public static Duration countDuration(LocalTime totalHoursForOneDay) {
+    private static Duration countDuration(LocalTime totalHoursForOneDay) {
         Duration totalDuration = Duration.ZERO;
-        totalDuration = Duration.ofHours(totalHoursForOneDay.getHour());
-        totalDuration = Duration.ofMinutes(totalHoursForOneDay.getMinute());
+        totalDuration = totalDuration.plusHours(totalHoursForOneDay.getHour());
+        totalDuration = totalDuration.plusMinutes(totalHoursForOneDay.getMinute());
         System.out.println("Duration in hours is " + totalDuration);
         return  totalDuration;
     }
