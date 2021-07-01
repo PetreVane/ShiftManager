93108d2cec11c345b8ebf43384502e4b1ea3bf10
Petre Vane <>
2021-07-01 11:52:11 +0200

    Added DashboardView and chart

---
+++ src/main/java/dk/project/shifter/ui/view/dashboard/DashboardView.java
@@ -0,0 +1,56 @@
+package dk.project.shifter.ui.view.dashboard;
+
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.ChartType;
+import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.splitlayout.SplitLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import dk.project.shifter.backend.service.ShiftService;
+import dk.project.shifter.ui.MainLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Route(value = "dashboard", layout = MainLayout.class)
+@PageTitle("Dashboard")
+public class DashboardView extends VerticalLayout {
+
+        private ShiftService shiftService;
+
+        public DashboardView (@Autowired ShiftService shiftService) {
+                this.shiftService = shiftService;
+                addClassName("dashboard-view");
+                setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+                add(getShiftStats(), getShiftsByType());
+        }
+
+        private Component getShiftStats() {
+                Span stats = new Span(shiftService.countShifts() + " shifts");
+                addClassName("shift-stats");
+                return stats;
+        }
+
+        private Chart getShiftsByType() {
+                Chart chart = new Chart(ChartType.PIE);
+                DataSeries dataSeries = new DataSeries();
+                var mappedShifts = shiftService.mapShiftsByType();
+                mappedShifts.forEach((shiftType, counter) ->
+                        dataSeries.add(new DataSeriesItem(shiftType, counter)));
+                chart.getConfiguration().setSeries(dataSeries);
+                return chart;
+        }
+
+        private SplitLayout addSummary(ShiftService shiftService) {
+                SplitLayout splitLayout = new SplitLayout();
+                splitLayout.setOrientation(SplitLayout.Orientation.VERTICAL);
+                splitLayout.addToPrimary(getShiftStats(), getShiftsByType());
+                splitLayout.addToSecondary(new SummaryView(shiftService));
+                return splitLayout;
+        }
+
+}
