04c12c8dcddca37e20b677a4784f9c3dc8941838
Petre Vane <>
2021-07-01 11:47:49 +0200

    Removed fields and made all methods static

--- src/main/java/dk/project/shifter/logic/Calculator.java
+++ src/main/java/dk/project/shifter/logic/Calculator.java
@@ -1,39 +1,38 @@
 package dk.project.shifter.logic;
 
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import dk.project.shifter.backend.entity.Shift;
 
-import java.time.LocalDate;
+import java.time.Duration;
 import java.time.LocalTime;
 
-@Data
-@RequiredArgsConstructor
 public class Calculator {
 
-    private LocalDate date;
-    private LocalTime startingTime;
-    private LocalTime endingTime;
-    private boolean hadBreak;
+    public static LocalTime getWorkedHoursFor(Shift shift) {
+        var totalHours = shift.getEndingTime().minusHours(shift.getStartingTime().getHour());
+        if (shift.hadBreak()) { totalHours = totalHours.minusMinutes(30); }
 
-    public LocalTime getWorkedHoursFor() {
-        var totalHours = endingTime.minusHours(startingTime.getHour());
-        if (hadBreak) {
-            totalHours = totalHours.minusMinutes(30);
-        }
-
-        var totalMinutes = Math.abs(totalHours.getMinute() - startingTime.getMinute());
+        var totalMinutes = Math.abs(totalHours.getMinute() - shift.getStartingTime().getMinute());
         totalHours = totalHours.minusMinutes(totalMinutes);
         LocalTime result = LocalTime.of(totalHours.getHour(), totalMinutes);
+        System.out.println("Total hours worked: " + result);
+        countDuration(result);
         return result;
     }
 
-    public int convertTime() {
-        LocalTime workDay = getWorkedHoursFor();
-        int totalHours = workDay.getHour();
-        int totalMinutes = workDay.getMinute();
-        var hoursConverted = totalMinutes / 60;
+    public static double countWorkedHours(LocalTime totalHoursForOneDay) {
+        double totalHours = totalHoursForOneDay.getHour();
+        double totalMinutes = totalHoursForOneDay.getMinute();
+        double hoursConverted = totalMinutes / 60;
         var extraMinutes = totalMinutes % 60;
         System.out.println("Total worked hours is " + totalHours + " hours and " + extraMinutes + " minutes");
         return totalHours = totalHours + hoursConverted;
+    }
+
+    public static void countDuration(LocalTime totalHoursForOneDay) {
+        Duration totalDuration = Duration.ZERO;
+        totalDuration = Duration.ofHours(totalHoursForOneDay.getHour());
+        totalDuration = Duration.ofMinutes(totalHoursForOneDay.getMinute());
+
+        System.out.println("Duration in hours is " + totalDuration);
     }
 }
