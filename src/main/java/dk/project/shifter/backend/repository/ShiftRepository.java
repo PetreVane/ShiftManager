fa44dd6ac4a4286c6eac96f2f1fd1bf1bce794d8
Petre Vane <>
2021-07-01 11:51:33 +0200

    Added new logic for fetching records based on Date argument type

--- src/main/java/dk/project/shifter/backend/repository/ShiftRepository.java
+++ src/main/java/dk/project/shifter/backend/repository/ShiftRepository.java
@@ -3,6 +3,15 @@ package dk.project.shifter.backend.repository;
 
 import dk.project.shifter.backend.entity.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
-public interface ShiftRepository extends JpaRepository<Shift, Long> { }
+public interface ShiftRepository extends JpaRepository<Shift, Long> {
+
+    @Query("select s from Shift s where s.shiftDate between :startDate and :endDate")
+    List<Shift> searchShift(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+}
